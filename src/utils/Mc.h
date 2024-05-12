41055b9e82e89c9d3f0a4c3222cdb5cbcaadf16b
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-05-12 21:05:51 +0800

    feat: sendText 支持更多重载

--- src/utils/Mc.h
+++ src/utils/Mc.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "Utils.h"
+#include "ll/api/Logger.h"
+#include "ll/api/service/Bedrock.h"
 #include "mc/world/actor/player/Player.h"
+#include "mc/world/level/Level.h"
 #include <mc/server/commands/CommandOutput.h>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
+
 
 namespace lbm::utils::mc {
 
@@ -23,7 +28,7 @@ inline static std::unordered_map<MsgLevel, string> Color = {
 
 // Template function sendText, usage: sendText() or sendText<MsgLevel::Success>().
 template <MsgLevel type = MsgLevel::Normal, typename... Args>
-inline void sendText(const Player& player, const string& fmt, Args&&... args) {
+inline void sendText(Player& player, const string& fmt, Args&&... args) {
     player.sendMessage(utils::format(MSG_TITLE + Color[type] + fmt, args...));
 }
 template <MsgLevel type = MsgLevel::Normal, typename... Args>
@@ -32,6 +37,35 @@ inline void sendText(CommandOutput& output, const string& fmt, Args&&... args) {
         output.error(utils::format(MSG_TITLE + Color[type] + fmt, args...));
     } else {
         output.success(utils::format(MSG_TITLE + Color[type] + fmt, args...));
+    }
+}
+template <MsgLevel type = MsgLevel::Normal, typename... Args>
+inline void sendText(Player* player, const string& fmt, Args&&... args) {
+    if (player) {
+        return sendText<type>(player, fmt, args...);
+    } else {
+        std::runtime_error("Failed in sendText: player is nullptr");
+    }
+}
+template <MsgLevel type = MsgLevel::Normal, typename... Args>
+inline void sendText(const string& realName, const string& fmt, Args&&... args) {
+    auto level = ll::service::getLevel();
+    if (level.has_value()) {
+        return sendText(level->getPlayer(realName), fmt, args...);
+    } else {
+        std::runtime_error("Failed in sendText: level is nullptr");
+    }
+}
+template <MsgLevel type = MsgLevel::Normal, typename... Args>
+inline void sendText(ll::Logger& logger, const string& fmt, Args&&... args) {
+    if constexpr (type == MsgLevel::Error) {
+        logger.error(utils::format(MSG_TITLE + Color[type] + fmt, args...));
+    } else if constexpr (type == MsgLevel::Fatal) {
+        logger.fatal(utils::format(MSG_TITLE + Color[type] + fmt, args...));
+    } else if constexpr (type == MsgLevel::Warn) {
+        logger.warn(utils::format(MSG_TITLE + Color[type] + fmt, args...));
+    } else {
+        logger.info(utils::format(MSG_TITLE + Color[type] + fmt, args...));
     }
 }
 
