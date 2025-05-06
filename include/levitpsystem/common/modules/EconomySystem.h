0f8532ca25ee617758d3e6a3de69fe2b3c4023fe
engsr6982 <pwxg286704@163.com>
2025-05-07 01:17:08 +0800

    Remove old assets and modules related to the LeviTPSystem Menu
    - Deleted the index.json file containing menu configurations.
    - Removed the zh_CN.json language file for Chinese translations.
    - Eliminated the Menu module (Menu.cc and Menu.h) which handled menu creation and interactions.
    - Removed the Date utility class that provided date handling functionalities.
    - Deleted the McUtils utility header that contained various helper functions for Minecraft interactions.
    - Removed the Utils header that provided string and vector utilities.
    - Added a new McUtils implementation file to replace the deleted header, consolidating utility functions for better organization.

--- include/levitpsystem/common/modules/EconomySystem.h
+++ include/levitpsystem/common/modules/EconomySystem.h
@@ -1,9 +1,10 @@
 #pragma once
+#include "levitpsystem/Global.h"
+#include "ll/api/base/StdInt.h"
 #include <memory>
 #include <mutex>
 #include <string>
 
-#include "levitpsystem/Global.h"
 
 class Player;
 namespace mce {
@@ -41,25 +42,25 @@ public:
     TPSNDAPI static std::shared_ptr<EconomySystem> getInstance();
 
 public:
-    TPSNDAPI virtual long long get(Player& player) const        = 0;
-    TPSNDAPI virtual long long get(mce::UUID const& uuid) const = 0;
+    TPSNDAPI virtual llong get(Player& player) const        = 0;
+    TPSNDAPI virtual llong get(mce::UUID const& uuid) const = 0;
 
-    TPSNDAPI virtual bool set(Player& player, long long amount) const        = 0;
-    TPSNDAPI virtual bool set(mce::UUID const& uuid, long long amount) const = 0;
+    TPSNDAPI virtual bool set(Player& player, llong amount) const        = 0;
+    TPSNDAPI virtual bool set(mce::UUID const& uuid, llong amount) const = 0;
 
-    TPSNDAPI virtual bool add(Player& player, long long amount) const        = 0;
-    TPSNDAPI virtual bool add(mce::UUID const& uuid, long long amount) const = 0;
+    TPSNDAPI virtual bool add(Player& player, llong amount) const        = 0;
+    TPSNDAPI virtual bool add(mce::UUID const& uuid, llong amount) const = 0;
 
-    TPSNDAPI virtual bool reduce(Player& player, long long amount) const        = 0;
-    TPSNDAPI virtual bool reduce(mce::UUID const& uuid, long long amount) const = 0;
+    TPSNDAPI virtual bool reduce(Player& player, llong amount) const        = 0;
+    TPSNDAPI virtual bool reduce(mce::UUID const& uuid, llong amount) const = 0;
 
-    TPSNDAPI virtual bool transfer(Player& from, Player& to, long long amount) const                   = 0;
-    TPSNDAPI virtual bool transfer(mce::UUID const& from, mce::UUID const& to, long long amount) const = 0;
+    TPSNDAPI virtual bool transfer(Player& from, Player& to, llong amount) const                   = 0;
+    TPSNDAPI virtual bool transfer(mce::UUID const& from, mce::UUID const& to, llong amount) const = 0;
 
 public:
-    TPSNDAPI virtual std::string getCostMessage(Player& player, long long amount) const;
+    TPSNDAPI virtual std::string getCostMessage(Player& player, llong amount) const;
 
-    TPSAPI virtual void sendNotEnoughMoneyMessage(Player& player, long long amount) const;
+    TPSAPI virtual void sendNotEnoughMoneyMessage(Player& player, llong amount) const;
 };
 
 
@@ -69,20 +70,20 @@ class LegacyMoneyEconomySystem final : public EconomySystem {
 public:
     TPSNDAPI explicit LegacyMoneyEconomySystem(EconomyConfig config);
 
-    TPSNDAPI long long get(Player& player) const override;
-    TPSNDAPI long long get(mce::UUID const& uuid) const override;
+    TPSNDAPI llong get(Player& player) const override;
+    TPSNDAPI llong get(mce::UUID const& uuid) const override;
 
-    TPSNDAPI bool set(Player& player, long long amount) const override;
-    TPSNDAPI bool set(mce::UUID const& uuid, long long amount) const override;
+    TPSNDAPI bool set(Player& player, llong amount) const override;
+    TPSNDAPI bool set(mce::UUID const& uuid, llong amount) const override;
 
-    TPSNDAPI bool add(Player& player, long long amount) const override;
-    TPSNDAPI bool add(mce::UUID const& uuid, long long amount) const override;
+    TPSNDAPI bool add(Player& player, llong amount) const override;
+    TPSNDAPI bool add(mce::UUID const& uuid, llong amount) const override;
 
-    TPSNDAPI bool reduce(Player& player, long long amount) const override;
-    TPSNDAPI bool reduce(mce::UUID const& uuid, long long amount) const override;
+    TPSNDAPI bool reduce(Player& player, llong amount) const override;
+    TPSNDAPI bool reduce(mce::UUID const& uuid, llong amount) const override;
 
-    TPSNDAPI bool transfer(Player& from, Player& to, long long amount) const override;
-    TPSNDAPI bool transfer(mce::UUID const& from, mce::UUID const& to, long long amount) const override;
+    TPSNDAPI bool transfer(Player& from, Player& to, llong amount) const override;
+    TPSNDAPI bool transfer(mce::UUID const& from, mce::UUID const& to, llong amount) const override;
 };
 
 // class ScoreBoardEconomySystem final : public EconomySystem {};
